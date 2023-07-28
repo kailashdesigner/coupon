3a18b96574cd97a0171ed72cb4c2c3dd13332ccf
kailash <kailash@logic-square.com>
2023-07-28 21:00:25 +0530

    all pages done

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,70 @@ $(".showContent").click(function () {
 });
 
 
+// coupon code
+$(document).ready(function () {
+  var modalBox = $("#exampleModal");
+  var modalTitle = modalBox.find(".titleNew");
+  var modalParaGrapf = modalBox.find(".content");
+  var modalOfferCode = modalBox.find(".Code");
+  var modalAnchor = modalBox.find(".anchor");
+  var buttons = $(".showButtonNew");
+  var pageTitle = $(".pageTitle h1").text();
+  buttons.click(function (value) {
+    var thisButton = $(this);
+    var title = $(this).parent().find("h1").text();
+    var content = $(this).parent().find("p").text();
+    var couponCode = $(this).find(".couponCode").text();
+    var weblocation = $(this).next("a").attr("href");
+
+    console.log(couponCode);
+
+    modalTitle.text(title);
+    modalParaGrapf.text(content);
+    modalOfferCode.text(couponCode);
+    modalAnchor.attr("href", weblocation);
+    $(".Code").hide();
+    $(".spinner").show();
+    // window.open(weblocation, "_blank");
+    setTimeout(function () {
+      $(thisButton).addClass("showCouponCode");
+      $(".Code").show();
+      $(".spinner").hide();
+    }, 4000);
+  });
+});
+
+
+$(document).ready(function () {
+        
+  function storeCardData(redirectItemContent, weblocation) {
+    var pageName = $(".innerTitleSection h1").text();
+    var pageLogo = $(".innerTitleSection img").attr("src");
+
+    // Create an array with the card data
+    var cardArray = [pageName, pageLogo, redirectItemContent, weblocation];
+
+    // Convert the array to a JSON string
+    var cardArrayJSON = JSON.stringify(cardArray);
+
+    // Store the JSON string in sessionStorage
+    sessionStorage.setItem("cardData", cardArrayJSON);
+  }
+
+  $(".btnPassData").on("click", function () {
+    var redirectItemContent = $(this).parent().find("p").text();
+    var weblocation = $(this).next("a").attr("href");
+    // console.log(redirectItemContent, weblocation);
+    // Store card data in sessionStorage
+    storeCardData(redirectItemContent, weblocation);
+
+    // Navigate to another page and pass data as query parameters
+    var targetPage = "../../redeem.html";
+    var urlWithQueryParams = targetPage + "?data=" + encodeURIComponent(sessionStorage.getItem("cardData")) ;
+    window.open(urlWithQueryParams, '_blank');
+  });
+});
+
 // banner slider
 var swiper = new Swiper(".myBannerSwiper", {
   slidesPerView: 3,
@@ -88,5 +152,3 @@ var swiper = new Swiper(".delas", {
     },
   },
 });
-
-
